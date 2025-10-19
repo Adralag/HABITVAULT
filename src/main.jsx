d682cc3f29d820f7aeacf9d93a2ab965bbe3db1b
Osaseye <sadebowale092@gmail.com>
2025-10-19 08:15:27 +0100

    Added PWA functionality

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './firebase/AuthContext'
+import './pwa'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
